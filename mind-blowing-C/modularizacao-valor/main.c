427fcfb21be73a86015506abb38f530c935e2b2e
Gabriel Victor <gabriel.victorc13@gmail.com>
2021-05-22 15:43:49 -0300

    ex05 - feito

--- mind-blowing-C/modularizacao-valor/main.c
+++ mind-blowing-C/modularizacao-valor/main.c
@@ -3,10 +3,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <math.h>
-
-#include <time.h>
-#include <ctype.h>
 #ifdef _WIN64
 #include <windows.h>
 void clear()
@@ -28,21 +24,8 @@ void clear()
 #endif
 
 #define RESET "\033[0m"
-#define BLACK "\033[30m"              /* Black */
-#define RED "\033[31m"                /* Red */
-#define GREEN "\033[32m"              /* Green */
-#define YELLOW "\033[33m"             /* Yellow */
-#define BLUE "\033[34m"               /* Blue */
-#define MAGENTA "\033[35m"            /* Magenta */
-#define CYAN "\033[36m"               /* Cyan */
-#define WHITE "\033[37m"              /* White */
-#define BOLDBLACK "\033[1m\033[30m"   /* Bold Black */
 #define BOLDRED "\033[1m\033[31m"     /* Bold Red */
 #define BOLDGREEN "\033[1m\033[32m"   /* Bold Green */
-#define BOLDYELLOW "\033[1m\033[33m"  /* Bold Yellow */
-#define BOLDBLUE "\033[1m\033[34m"    /* Bold Blue */
-#define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
-#define BOLDCYAN "\033[1m\033[36m"    /* Bold Cyan */
 #define BOLDWHITE "\033[1m\033[37m"   /* Bold White */
 
 #define EX05_ARRAY_LENGTH 3
@@ -234,9 +217,10 @@ int ex04()
 /*========================================================================*/
 
 int fatorial(int numero){
-    int fatorial = 0;
+    float fatorial = 1;
     for(int i = numero; i > 0 ; i--){
         fatorial *= numero;
+        numero--;
     }
     return fatorial;
 }
@@ -248,5 +232,5 @@ int ex05(){
     printf("Digite uma numero para que seja calculado seu fatorial: ");
     scanf("%d", &numero);
     int FAT = fatorial(numero);
-    printf("\nO fatorial de %d é: [ %d ]", numero, FAT);
+    printf(BOLDGREEN"\nO fatorial de %d é: [ %d ]"RESET, numero, FAT);
 }
